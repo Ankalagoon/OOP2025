2df52fa61daca4c9b1920bd2ef4d473b699cc181
Ankalagoon <georgealexandrutanasuca@gmail.com>
2025-04-28 19:06:10 +0300

    Lab 8 update

--- Lab8/main.cpp
+++ Lab8/main.cpp
@@ -2,36 +2,43 @@
 #include <string>
 #include <map>
 #include <queue>
+#include <fstream>
 using namespace std;
 
-struct dub {
-	int x;
-	string s;
-	bool operator <(const dub& a) const{
-		if (x == a.x) return s > a.s;
-		return x < a.x;
+ifstream fin("input.txt");
+
+class Comp 
+{
+public:
+	bool operator() (const pair<int,string>& x, const pair<int, string>& y)const {
+		if (x.first == y.first) return x.second > y.second;
+		return x.first < y.first;
 	}
+	
 };
+
+
 
 int main() {
 	map<string,int> M;
-	priority_queue<dub> pq;
+	priority_queue<pair<int,string>,vector<pair<int, string>>,Comp> pq;
 	int i, j;
 	char c;
 	string text;
-	getline(cin, text);
+	getline(fin, text);
 	i = 0;
 	j = 0;
 	for (i=0;i<text.size();i++){
 		c = text[i];
 		if (c >= 65 && c <= 90) text[i] += 32;
-		if (c == ' ' || c == ',' || c == '!' || c == '?' || c == '.') {
+		if (c == ' ' || c == ',' || c == '!' || c == '?' || c == '.') {	
 			if (j != i) {
 				M[text.substr(j, i - j)]++;
 			}
 			j= i + 1;
 		}
 	}
+
 	if (j != i) {
 		M[text.substr(j, i - 1)]++;
 	}
@@ -39,7 +46,7 @@ int main() {
 		pq.push({ w.second,w.first });
 	}
 	while (!pq.empty()) {
-		cout << pq.top().s << " => " << pq.top().x << "\n";
+		cout << pq.top().second << " => " << pq.top().first << "\n";
 		pq.pop();
 	}
 }
